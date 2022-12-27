7c6fe5dac0bccc06fa080855ef868a188276b14b
Mohammad Mahdi Nemati <nemati.mahdi255@gmail.com>
2022-12-27 23:23:58 +0330

    add javascript

---
+++ JS/script.js
@@ -0,0 +1,74 @@
+const accountImg = document.querySelector("img");
+const info = document.querySelector("#info");
+const bio = document.querySelector("#bio");
+const usernameInput = document.querySelector(".username");
+const submitBotton = document.querySelector(".submit");
+
+function submit() {
+    // username = "1995parham";
+
+    fetch(`https://api.github.com/users/${usernameInput.value}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
+    .then(response => {
+        if(response.status == 200) {
+            setProfile(response)
+        }
+    });
+
+}
+
+function setProfile(response) {
+    response.json()
+    .then(obj => {
+        console.log(obj);
+        accountImg.src = obj.avatar_url;
+        info.innerHTML =`
+        <p>
+        Name: ${obj.name == "" ? "Unset" : obj.name}
+        </p>
+        <p>
+        Blog: ${obj.blog == "" ? "Unset" : obj.blog}
+        </p>
+        <p>
+        Location: ${obj.location == null ? "Unset" : obj.location}
+        </p>
+        `
+        bio.innerHTML = `<p>${obj.bio}</p>`.replace("\n", "<br />");
+
+        fetch(`https://api.github.com/users/${usernameInput.value}/repos`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .then(obj => {
+            obj.sort((a, b) => {
+                return new Date(b.pushed_at).getTime() - new Date(a.pushed_at).getTime();
+            })
+            console.log(obj);
+            let firstFiveRepo = obj.slice(0, 5);
+            console.log(firstFiveRepo);
+        });
+
+    });
+}
+
+function checkValidity(name) {
+    const regex1 = /[A-Za-z ]+/g;
+    const regex2 = /[0-9\.\-\/]+/g;
+    const foundValid = name.match(regex1);
+    const foundNotValid = name.match(regex2);
+    if (foundNotValid == null && foundValid.length > 0) {
+        return true;
+    }
+    return false;
+
+}
+
+submitBotton.addEventListener("click", submit);
+window.localStorage.clear();
